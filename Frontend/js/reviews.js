2f9972ad158c425db0cc017669dd5c3b47c746ff
Esteban <eordonez@fi.uba.ar>
2026-02-02 22:27:41 -0300

    Se cambio todas las funciones y variables que poseian perfil por review

--- Frontend/js/reviews.js
+++ Frontend/js/reviews.js
@@ -1,4 +1,4 @@
-const cardContainer = document.getElementById('grid-tarjetas-perfil');
+const cardContainer = document.getElementById('grid-tarjetas-review');
 
 
 async function cargarCard() {
@@ -6,7 +6,7 @@ async function cargarCard() {
     try{
     cardContainer.innerHTML = '<p>Cargando datos del servidor simulado...</p>';
 
-    //const response = await fetch('../js/data/perfil.json');
+    const response = await fetch('../js/data/perfil.json');
 
     if(!response.ok){
         throw new Error('No se pudo conectar con el servidor');
@@ -14,7 +14,7 @@ async function cargarCard() {
     const datos = await response.json();
     await new Promise(resolve => setTimeout(resolve, 500));
 
-    renderizarPerfiles(datos);
+    renderizarReviews(datos);
 
     } catch (error){
         console.error("Error critico: ", error);
@@ -24,82 +24,82 @@ async function cargarCard() {
 }
 
 
-function renderizarPerfiles(perfiles){
+function renderizarReviews(reviews){
     cardContainer.innerHTML = ' ';
 
-    perfiles.forEach(perfil => {
+    reviews.forEach(review => {
             const cardHTML = `
-                <div class="masonry-item" data-id="${perfil.id}">
+                <div class="masonry-item" data-id="${review.id}">
                     <div class="card">
 
                         <div class="card-content has-text-centered">
 
                             <figure class="image is-128x128 is-inline-block mb-3">
                                 <img class="author-avatar"
-                                    src="${perfil.fotoPerfil}"
-                                    alt="Foto de ${perfil.nombre} ${perfil.apellido}">
+                                    src="${review.fotoPerfil}"
+                                    alt="Foto de ${review.nombre} ${review.apellido}">
                             </figure>
 
-                            <p class="title is-5 mb-1">${perfil.nombre} ${perfil.apellido}</p>
+                            <p class="title is-5 mb-1">${review.nombre} ${review.apellido}</p>
 
-                            <p class="is-size-7 has-text-grey mb-2">${perfil.carrera}</p>
+                            <p class="is-size-7 has-text-grey mb-2">${review.carrera}</p>
 
-                            <p class="is-size-7 mb-2">Aura: ${perfil.aura}</p>
+                            <p class="is-size-7 mb-2">Aura: ${review.aura}</p>
 
                             <p class="is-size-7">
-                                <a href="mailto:${perfil.email}">${perfil.email}</a>
+                                <a href="mailto:${review.email}">${review.email}</a>
                             </p>
 
                         </div>
 
                         <footer class="card-footer">
-                            <a class="card-footer-item button is-white is-small" onclick="confirmacionEliminarPerfil(${perfil.id})">Eliminar Perfil</a>
-                            <a class="card-footer-item button is-white is-small" onclick="abrirPopupModificarPerfil(${perfil.id})">Modificar Perfil</a>
+                            <a class="card-footer-item button is-white is-small" onclick="confirmacionEliminarReview(${review.id})">Eliminar review</a>
+                            <a class="card-footer-item button is-white is-small" onclick="abrirPopupModificarReview(${review.id})">Modificar review</a>
                             <a class="card-footer-item button is-white is-small">Crear Review</a>
                         </footer>
 
                     </div>
                     
-                    <div class="popup-overlay" id="popupOverlayModificarPerfil${perfil.id}">
+                    <div class="popup-overlay" id="popupOverlayModificarReview${review.id}">
                         <div class="popup-content" id="popupContent">
-                            <h2 class="tituloPopup">Modificar Perfil</h2>
-                            <form id="formModificarPerfil">
+                            <h2 class="tituloPopup">Modificar review</h2>
+                            <form id="formModificarReview">
                                 <div class="field">
                                     <label class="label" for="Nombre">Nombre:</label>
                                     <div class="control">
-                                        <input class="input" type="text" id="Nombre" value="${perfil.nombre}" name="Nombre" required>
+                                        <input class="input" type="text" id="Nombre" value="${review.nombre}" name="Nombre" required>
                                     </div>
                                 </div>
                                 <div class="field">
                                     <label class="label" for="Apellido">Apellido:</label>
                                     <div class="control">
-                                        <input class="input" type="text" id="Apellido" value="${perfil.apellido}" name="Apellido" required>
+                                        <input class="input" type="text" id="Apellido" value="${review.apellido}" name="Apellido" required>
                                     </div>
                                 </div>
                                 <div class="field">
                                     <label class="label" for="Carrera">Carrera:</label>
                                     <div class="control">
-                                        <input class="input" type="text" id="Carrera" value="${perfil.carrera}" name="Carrera" required>
+                                        <input class="input" type="text" id="Carrera" value="${review.carrera}" name="Carrera" required>
                                     </div>
                                 </div>
                                 <div class="field">
                                     <label class="label" for="Email">Email:</label>
                                     <div class="control">
-                                        <input class="input" type="email" id="Email" value="${perfil.email}" name="Email" required>
+                                        <input class="input" type="email" id="Email" value="${review.email}" name="Email" required>
                                     </div>
                                 </div>
                                 <div class="field">
-                                    <label class="label" for="Foto">Foto de Perfil:</label>
+                                    <label class="label" for="Foto">Foto de review:</label>
                                     <div class="control">
-                                        <input class="input" type="url" id="Foto" value="${perfil.fotoPerfil}" name="Foto" required>
+                                        <input class="input" type="url" id="Foto" value="${review.fotoreview}" name="Foto" required>
                                     </div>
                                 </div>
                             </form>
                             <div class="botones-popup">
-                                <button class="button botonPopup is-link is-normal" onclick="cerrarPopupModificarPerfil(${perfil.id})">
+                                <button class="button botonPopup is-link is-normal" onclick="cerrarPopupModificarReview(${review.id})">
                                     Cancelar
                                 </button>
-                                <button class="button botonPopup is-link is-normal" type="submit" id="botonModificar" form="formModificarPerfil">
+                                <button class="button botonPopup is-link is-normal" type="submit" id="botonModificar" form="formModificarReview">
                                     Modificar
                                 </button>
                             </div>
@@ -113,16 +113,16 @@ function renderizarPerfiles(perfiles){
 }
 
 
-function abrirPopupCrearPerfil() {
-    const popup = document.getElementById('popupOverlayCrearPerfil');
+function abrirPopupCrearReview() {
+    const popup = document.getElementById('popupOverlayCrearReview');
     if (popup) {
         popup.style.display = 'flex';
     }
 }
 
-function cerrarPopupCrearPerfil() {
-    const popup = document.getElementById('popupOverlayCrearPerfil');
-    const form = document.getElementById('formCrearPerfil');
+function cerrarPopupCrearReview() {
+    const popup = document.getElementById('popupOverlayCrearReview');
+    const form = document.getElementById('formCrearReview');
     if (form) {
         form.reset();
     }
@@ -131,16 +131,16 @@ function cerrarPopupCrearPerfil() {
     }
 }
 
-function abrirPopupModificarPerfil(id) {
-    const popup = document.getElementById('popupOverlayModificarPerfil' + id);
+function abrirPopupModificarReview(id) {
+    const popup = document.getElementById('popupOverlayModificarReview' + id);
     if (popup) {
         popup.style.display = 'flex';
     }
 }
 
-function cerrarPopupModificarPerfil(id) {
-    const popup = document.getElementById('popupOverlayModificarPerfil' + id);
-    const form = document.getElementById('formModificarPerfil');
+function cerrarPopupModificarReview(id) {
+    const popup = document.getElementById('popupOverlayModificarReview' + id);
+    const form = document.getElementById('formModificarReview');
     if (form) {
         form.reset();
     }
@@ -149,17 +149,17 @@ function cerrarPopupModificarPerfil(id) {
     }
 }
 
-function confirmacionEliminarPerfil(id) {
-    const confirmar = confirm("¿Estás seguro de que deseas eliminar este perfil?");
+function confirmacionEliminarReview(id) {
+    const confirmar = confirm("¿Estás seguro de que deseas eliminar este review?");
     if (confirmar) {
-        eliminarPerfil(id);
+        eliminarReview(id);
     }
 }
 
-function eliminarPerfil(id) {
-    const perfilElement = document.querySelector(`.masonry-item[data-id='${id}']`);
-    if (perfilElement) {
-        perfilElement.remove();
+function eliminarReview(id) {
+    const reviewElement = document.querySelector(`.masonry-item[data-id='${id}']`);
+    if (reviewElement) {
+        reviewElement.remove();
     }
 }
 
