6786907d7aea79490ddca680aa29a430b00d6f9c
Arleys Camano <anino@fi.uba.ar>
2026-02-05 12:04:00 -0300

    Se arreglo vista movil y se agrego menu hamburguesa para version movil y tablet

--- Frontend/js/reviews.js
+++ Frontend/js/reviews.js
@@ -258,3 +258,22 @@ async function eliminarReview(id_review, boton) {
         alert(error.message); 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if ($navbarBurgers.length > 0) {
+
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+});
