62b74b6c20f108112fdbaf2ef4846ca4fb76b80a
Esteban <eordonez@fi.uba.ar>
2026-02-05 04:09:00 -0300

    Se ordeno la integracion

--- Frontend/js/reviews.js
+++ Frontend/js/reviews.js
@@ -25,6 +25,52 @@ async function cargarUsuarios() {
 }
 cargarUsuarios();
 
+async function cargarCard() {
+    try{
+    const response = await fetch('http://localhost:3000/api/reviews');
+    const datos = await response.json();
+    renderizarReviews(datos);
+    } catch (error){
+        console.error("Error critico: ", error);
+        cardContainer.innerHTML = `<p style = "color:red"> Error al cargar los datos: ${error.message}</p>`;
+    }
+}
+
+cargarCard();
+
+function renderizarReviews(reviews){
+    cardContainer.innerHTML = ' ';
+    let htmlAcomulado ='';
+    reviews.forEach(review => {
+        const cardHTML = `
+            <div class="masonry-item" data-id="${review.id_review}">
+                <div class="card">
+                    <div class="card-content has-text-centered">
+                        <p class="content is-size-6 has-text-black mb-4">
+                            ${review.descripcion}
+                        </p>
+                        <p class="is-size-7 mb-2">Aura: ${review.aura}</p>
+                    </div>
+                    <div class="media is-vcentered has-text-centered border-top pt-1 mb-2 footer-card">
+                        <div class="media-content">
+                            <figure class="image is-32x32 is-inline-block">
+                                <img class="author-avatar" src="${review.puntuador.foto_user}" alt="Avatar">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="is-size-6 has-text-weight-semibold mt-2 has-text-dark">${review.puntuador.nombre} ${review.puntuador.apellido}</p>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item button is-white is-small" onclick="confirmacionEliminarReview(${review.id_review}, this)">Eliminar review</a>
+                    </footer>
+                </div>
+            </div>
+        `;
+        cardContainer.innerHTML += cardHTML;
+    });
+}
+
 function cerrarPopupCrearReview() {
     const popup = document.getElementById('popupOverlayCrearReview');
     const form = document.getElementById('formCrearReview');
@@ -65,7 +111,6 @@ function manejarBuscador(input, lista, inputOculto) {
 
 form.addEventListener('submit', async function(event) {
   event.preventDefault();
-
 
   const puntuadoId = inputOcultoPuntuado.value;
   const puntuadorId = inputOcultoPuntuador.value;
@@ -178,24 +223,6 @@ manejarBuscador(
   inputOcultoPuntuador
 );
 
-function abrirPopupModificarReview(id) {
-    const popup = document.getElementById('popupOverlayModificarReview' + id);
-    if (popup) {
-        popup.style.display = 'flex';
-    }
-}
-
-function cerrarPopupModificarReview(id) {
-    const popup = document.getElementById('popupOverlayModificarReview' + id);
-    const form = document.getElementById('formModificarReview');
-    if (form) {
-        form.reset();
-    }
-    if (popup) {
-        popup.style.display = 'none';
-    }
-}
-
 function confirmacionEliminarReview(id_review, boton) {
     const confirmar = confirm("¿Estás seguro de que deseas eliminar este review?");
     if (confirmar) {
@@ -225,95 +252,3 @@ async function eliminarReview(id_review, boton) {
         alert(error.message); 
     }
 }
-
-function renderizarReviews(reviews){
-    cardContainer.innerHTML = ' ';
-    let htmlAcomulado ='';
-    reviews.forEach(review => {
-        const cardHTML = `
-            <div class="masonry-item" data-id="${review.id_review}">
-                <div class="card">
-                    <div class="card-content has-text-centered">
-                        <p class="content is-size-6 has-text-black mb-4">
-                            ${review.descripcion}
-                        </p>
-                        <p class="is-size-7 mb-2">Aura: ${review.aura}</p>
-                    </div>
-                    <div class="media is-vcentered has-text-centered border-top pt-1 mb-2 footer-card">
-                        <div class="media-content">
-                            <figure class="image is-32x32 is-inline-block">
-                                <img class="author-avatar" src="${review.puntuador.foto_user}" alt="Avatar">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="is-size-6 has-text-weight-semibold mt-2 has-text-dark">${review.puntuador.nombre} ${review.puntuador.apellido}</p>
-                        </div>
-                    </div>
-                    <footer class="card-footer">
-                        <a class="card-footer-item button is-white is-small" onclick="confirmacionEliminarReview(${review.id_review}, this)">Eliminar review</a>
-                        <a class="card-footer-item button is-white is-small" onclick="abrirPopupModificarReview(${review.id_review})">Modificar review</a>
-                    </footer>
-                </div>
-                <div class="popup-overlay" id="popupOverlayModificarReview${review.id_review}">
-                    <div class="popup-content" id="popupContent">
-                        <h2 class="tituloPopup">Modificar review</h2>
-                        <form id="formModificarReview">
-                            <div class="field">
-                                <label class="label" for="Nombre">Nombre:</label>
-                                <div class="control">
-                                    <input class="input" type="text" id="Nombre" value="${review.nombre}" name="Nombre" required>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="label" for="Apellido">Apellido:</label>
-                                <div class="control">
-                                    <input class="input" type="text" id="Apellido" value="${review.apellido}" name="Apellido" required>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="label" for="Carrera">Carrera:</label>
-                                <div class="control">
-                                    <input class="input" type="text" id="Carrera" value="${review.carrera}" name="Carrera" required>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="label" for="Email">Email:</label>
-                                <div class="control">
-                                    <input class="input" type="email" id="Email" value="${review.email}" name="Email" required>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="label" for="Foto">Foto de review:</label>
-                                <div class="control">
-                                    <input class="input" type="url" id="Foto" value="${review.fotoreview}" name="Foto" required>
-                                </div>
-                            </div>
-                        </form>
-                        <div class="botones-popup">
-                            <button class="button botonPopup is-link is-normal" onclick="cerrarPopupModificarReview(${review.id})">
-                                Cancelar
-                            </button>
-                            <button class="button botonPopup is-link is-normal" type="submit" id="botonModificar" form="formModificarReview">
-                                Modificar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        cardContainer.innerHTML += cardHTML;
-    });
-}
-
-async function cargarCard() {
-    try{
-    const response = await fetch('http://localhost:3000/api/reviews');
-    const datos = await response.json();
-    renderizarReviews(datos);
-    } catch (error){
-        console.error("Error critico: ", error);
-        cardContainer.innerHTML = `<p style = "color:red"> Error al cargar los datos: ${error.message}</p>`;
-    }
-}
-
-cargarCard();
